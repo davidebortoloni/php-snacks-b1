2ab95ff36820486f7bf8677a6d6f6deff2e22d1f
davidebortoloni <davidebortoloni@gmail.com>
2021-09-23 19:43:13 +0200

    Sistemazione index

--- index.php
+++ index.php
@@ -16,6 +16,33 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/Projects/php-snacks-b1/components/hea
     // Header
     include_once($_SERVER['DOCUMENT_ROOT'] . '/Projects/php-snacks-b1/components/header.php');
     ?>
+    <main class="my-4">
+        <div class="container">
+            <div class="row justify-content-center g-5">
+                <div class="col-6">
+                    <a class="snack-link" href="http://localhost/Projects/php-snacks-b1/views/random_numbers.php">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            r4nd0m_num83r5
+                        </div>
+                    </a>
+                </div>
+                <div class="col-6">
+                    <a class="snack-link" href="http://localhost/Projects/php-snacks-b1/views/student_reports.php">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            5tud3nt_r3p0rt5
+                        </div>
+                    </a>
+                </div>
+                <div class="col-6">
+                    <a class="snack-link" href="http://localhost/Projects/php-snacks-b1/views/fake_login.php">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            f4k3_l0g1n
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
